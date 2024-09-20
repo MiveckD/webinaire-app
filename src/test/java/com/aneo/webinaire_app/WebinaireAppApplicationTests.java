d5c82911e45a5e1e72f688cf9aa9242253c8d1a3
Miveck DJONA (ANEO) <mdjona@aneo.fr>
2024-09-20 11:59:57 +0200

    Initial commit

---
+++ src/test/java/com/aneo/webinaire_app/WebinaireAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aneo.webinaire_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebinaireAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
